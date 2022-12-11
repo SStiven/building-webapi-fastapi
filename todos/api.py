76b4d668891649cb1a44d86317e9db16cfbfd46a
Stiven Villegas <svillegasc@protonmail.com>
2022-12-11 18:37:10 -0500

    Create multiple route app

--- todos/api.py
+++ todos/api.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from todo import todo_router
 
-app = FastAPI()
-
+app= FastAPI()
 
 @app.get("/")
 async def welcome() -> dict:
-	return { "message": "Hello world"}
+	return { "message": "Hello"}
+
+app.include_router(todo_router)
